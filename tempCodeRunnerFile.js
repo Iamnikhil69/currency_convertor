2ac4d29c039930fc8f0bce63aa3fc36286e0ef25
Iamnikhil69 <nikhil231130cse@sdbc.ac.in>
2026-08-27 10:41:15 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,8 @@
+const BASE_URL = "https://latest.currency-api.pages.dev/v1/currencies/eur.json";
+
+const dropdowns = document.querySelectorAll(".dropdown select");
+
+
+for (code in countryList){
+    console.log(code, countryList[code]);
+}
